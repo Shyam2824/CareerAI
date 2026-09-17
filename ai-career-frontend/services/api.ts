cdc03f23ed78ee64bfef11e7c4733142cce36054
Shyam2824 <97013528+Shyam2824@users.noreply.github.com>
2026-09-17 12:09:22 +0530

    Fix backend url

--- ai-career-frontend/services/api.ts
+++ ai-career-frontend/services/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API_URL =
   process.env.NEXT_PUBLIC_API_URL ||
-  "http://127.0.0.1:8000";
+  " https://careerai-5-mn6f.onrender.com";
 
 const api = axios.create({
   baseURL: API_URL,
